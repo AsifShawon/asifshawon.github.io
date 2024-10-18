22d43719b7d434fc4a55607588e895a82b4e9507
AsifShawon <asifbhshawon71@gmail.com>
2024-10-18 19:48:40 +0600

    edited menu

--- src/app/comps/menu.tsx
+++ src/app/comps/menu.tsx
@@ -1,45 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import { FolderCode, FileUser, FilePenLine, ContactRound } from 'lucide-react'; // import your icon components
 
-const Menu = () => {
-    return (
-        <div>
-            <div className="flex flex-col sm:flex-row gap-8 pt-5 text-xl sm:grid-cols-4">
-                <Link href="/hello/projects" className="relative group hover:text-blue-500">
-                    <span className="group-hover:pr-8 transition-all duration-300 ease-in-out">
-                        /Projects
-                    </span>
-                    <span className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:right-[-2px] transition-all duration-300 ease-in-out">
-                        ➜
-                    </span>
-                </Link>
-                <Link href="/hello/aboutme" className="relative group hover:text-blue-500">
-                    <span className="group-hover:pr-8 transition-all duration-300 ease-in-out">
-                        /About me
-                    </span>
-                    <span className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:right-[-2px] transition-all duration-300 ease-in-out">
-                        ➜
-                    </span>
-                </Link>
-                <Link href="" className="relative group hover:text-blue-500">
-                    <span className="group-hover:pr-8 transition-all duration-300 ease-in-out">
-                        /Blog
-                    </span>
-                    <span className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:right-[-2px] transition-all duration-300 ease-in-out">
-                        ➜
-                    </span>
-                </Link>
-                <Link href="" className="relative group hover:text-blue-500">
-                    <span className="group-hover:pr-8 transition-all duration-300 ease-in-out">
-                        /Contact
-                    </span>
-                    <span className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:right-[-2px] transition-all duration-300 ease-in-out">
-                        ➜
-                    </span>
-                </Link>
+export default function NavigationLinks() {
+  const links = [
+    {
+      href: "/hello/projects",
+      label: "Projects",
+      icon: <FolderCode size={24} />,
+    },
+    {
+      href: "/hello/aboutme",
+      label: "About me",
+      icon: <FileUser size={24} />,
+    },
+    {
+      href: "/hello/blog",
+      label: "Blog",
+      icon: <FilePenLine size={24} />, // No icon for this one
+    },
+    {
+      href: "/hello/contact",
+      label: "Contact",
+      icon: <ContactRound size={24}/>, // No icon for this one
+    },
+  ];
+
+  return (
+    <div>
+      <div className="flex gap-8 pt-5 text-xl">
+        {links.map((link, index) => (
+          <Link key={index} href={link.href} className="relative group hover:text-blue-500">
+            <div className="flex justify-normal gap-1 items-center">
+              {link.icon}
+              <span className="group-hover:pr-8 transition-all duration-300 ease-in-out sm:block hidden">
+                {link.label}
+              </span>
+              <span className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:right-[-2px] transition-all duration-300 ease-in-out">
+                ➜
+              </span>
             </div>
-        </div>
-    )
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default Menu
